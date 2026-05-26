fb1c5098b8bf352a1f94574ddf7935b889f9cdfe
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-27 01:41:18 +0530

    made the POST api in the self to create the notes

--- self/src/app.js
+++ self/src/app.js
@@ -1,12 +1,61 @@
 import express from "express";
 import { config } from "dotenv";
 import connecDB from "./config/db.config.js";
+import notesModel from "./models/notes.model.js";
 
 config();
 
 const app = express();
 app.use(express.json());
 await connecDB();
+
+/*
+@routeUsage: to crete a new note
+@routeType: POST because of creation
+@dataAccepted as raw json from the body
+*/
+app.post("/api/notes", async (req, res) => {
+
+    // accepting the data
+    let { title, description } = req.body;
+
+    // validations for the inputs
+    if (!title) {
+        res.status(400).json({
+            message: "Title is required"
+        });
+    }
+
+    if (!description) {
+        res.status(400).json({
+            message: "Description is required"
+        });
+    }
+
+    if (title.trim().length < 4) {
+        res.status(400).json({
+            message: "Title must be atleast 4 charactes long"
+        });
+    }
+
+    if (description.trim().length < 10) {
+        res.status(400).json({
+            message: "Description must be 10 characters long"
+        });
+    }
+
+    // adding in the DB
+    const notes = await notesModel.create({ title, description });
+
+    // sending the response 
+    return res.status(201).json({
+        success: true,
+        message: "Notes created successfully",
+        data: {
+            notes
+        }
+    });
+});
 
 
 export default app;
