cd30357579f59991241012042cdf2022badc533f
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-27 01:24:29 +0530

    Folders initalized with project and imrpoved strucutre

--- server.js
+++
@@ -1,5 +0,0 @@
-import app from "./src/app.js";
-
-app.listen(5000, () => {
-    console.log("Server Running Successfull on port 5000");
-})
--- src/config/db.config.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { mongo } from "mongoose";
-
-const noteSchema = await mongoose.Schema({
-    title: String,
-    description: String
-});
-
-const nodeModel = await mongoose.model("notes", noteSchema);
-export default nodeModel;
